199b54be649f073b6c022d2b548500807a4561d2
Matt Hawkins <trumpet580@gmail.com>
2011-10-04 17:42:14 -0400

    Added javadoc explaining what the class is for

--- src/com/kelsonprime/oregontrail/gui/Listener.java
+++ src/com/kelsonprime/oregontrail/gui/Listener.java
@@ -1,5 +1,12 @@
 package com.kelsonprime.oregontrail.gui;
-
+/**
+ *
+ * this class is the go between for the logic and the gui.
+ * When something changes in the logic, it tells the Listener (by calling some method in the listener)
+ * The Listener then updates the gui (in whatever manner we implement)
+ * @author Matt
+ *
+ */
 public class Listener {
-
+	
 }
