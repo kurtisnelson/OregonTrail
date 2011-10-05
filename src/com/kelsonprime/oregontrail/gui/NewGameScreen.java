49f19f6db80ecbabf6b71592d940304a50c28f8e
Matt Hawkins <trumpet580@gmail.com>
2011-10-05 01:37:45 -0400

    merging changes the NewGameScreen. I think...

--- src/com/kelsonprime/oregontrail/gui/NewGameScreen.java
+++ src/com/kelsonprime/oregontrail/gui/NewGameScreen.java
@@ -1,7 +1,13 @@
 package com.kelsonprime.oregontrail.gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class NewGameScreen extends JFrame {
+	
+	public NewGameScreen(String name){
+		super(name);
+	}
 
 }
