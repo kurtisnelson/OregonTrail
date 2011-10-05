6c596aa37565748d4a99f83d78cc1fc32f7fa013
Matt Hawkins <trumpet580@gmail.com>
2011-10-05 00:57:52 -0400

    Created MenuListener class. added menu listener to the newGame JMenuItem. Still doesn't actually do anything.

---
+++ src/com/kelsonprime/oregontrail/gui/MenuListener.java
@@ -0,0 +1,22 @@
+package com.kelsonprime.oregontrail.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class MenuListener implements ActionListener, ItemListener {
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/kelsonprime/oregontrail/gui/OregonTrail.java
+++ src/com/kelsonprime/oregontrail/gui/OregonTrail.java
@@ -1,5 +1,10 @@
 package com.kelsonprime.oregontrail.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -19,7 +24,12 @@ public class OregonTrail{
 		menuBar.add(fileMenu);
 		
 		JMenuItem newGame = new JMenuItem("New Game");
+		
 		fileMenu.add(newGame);
+		
+		MenuListener menuListen = new MenuListener();
+		newGame.addActionListener(menuListen);
+		
 		
 		JFrame frame = new JFrame("Oregon Trail");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,6 +38,5 @@ public class OregonTrail{
 		//frame.add(p);
 		frame.pack();
 		frame.setVisible(true);
-	}
-	
+	}		
 }
