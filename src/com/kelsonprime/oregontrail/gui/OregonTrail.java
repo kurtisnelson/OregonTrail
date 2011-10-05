d44012cb7e9927122da32c19b2a6914e8e106380
Matt Hawkins <trumpet580@gmail.com>
2011-10-05 00:32:59 -0400

    Added a JMenuBar. It has a File menu, which has a new Game menu item. They don't do anything yet.

--- src/com/kelsonprime/oregontrail/gui/OregonTrail.java
+++ src/com/kelsonprime/oregontrail/gui/OregonTrail.java
@@ -1,5 +1,10 @@
 package com.kelsonprime.oregontrail.gui;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  * This class is going to be the main game. It is in charge of owning the parent JFrame, and adding the initial menu to it
  * @author kurt
@@ -9,6 +14,20 @@ public class OregonTrail{
 	
 	public static void main(String[] args){
 		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		
+		JMenuItem newGame = new JMenuItem("New Game");
+		fileMenu.add(newGame);
+		
+		JFrame frame = new JFrame("Oregon Trail");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setJMenuBar(menuBar);
+		
+		//frame.add(p);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 }
