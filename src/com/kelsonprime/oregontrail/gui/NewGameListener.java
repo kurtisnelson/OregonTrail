20b2513e61eb326be7abfa33925f929f87f21244
Matt Hawkins <trumpet580@gmail.com>
2011-10-05 18:48:18 -0400

    created an ActionListener class for the newGame screen. Doesn't do anything yet.

---
+++ src/com/kelsonprime/oregontrail/gui/NewGameListener.java
@@ -0,0 +1,14 @@
+package com.kelsonprime.oregontrail.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NewGameListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+
+}
