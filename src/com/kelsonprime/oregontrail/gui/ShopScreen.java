3240460b768143f747693d37bf2401d8d092c536
jgifpeg <jgifpeg@yahoo.com>
2011-10-06 10:21:52 -0400

    Added ShopScreen Class

---
+++ src/com/kelsonprime/oregontrail/gui/ShopScreen.java
@@ -0,0 +1,14 @@
+package com.kelsonprime.oregontrail.gui;
+
+import java.awt.*;
+import javax.swing.*;
+import com.kelsonprime.oregontrail.logic.*;
+
+public class ShopScreen extends JPanel{
+	
+	Shop shop;
+	
+	public ShopScreen(Shop shop){
+		this.shop = shop;
+	}
+}
