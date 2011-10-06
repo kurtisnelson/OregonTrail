5bd5f533d32ea3e7b86b0047b681f89139af7955
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-06 14:46:19 -0400

    Made open() and exit() hooks

--- src/com/kelsonprime/oregontrail/gui/OregonTrail.java
+++ src/com/kelsonprime/oregontrail/gui/OregonTrail.java
@@ -2,22 +2,14 @@ package com.kelsonprime.oregontrail.gui;
 
 import com.kelsonprime.oregontrail.logic.Game;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 /**
  * This class is going to be the main game. It is in charge of owning the parent
  * JFrame, and adding the initial menu to it
- * 
- * @author kurt
  * 
  */
 public class OregonTrail {
@@ -35,21 +27,27 @@ public class OregonTrail {
 	}
 	
 	public static void main(String[] args) {
+		// TODO Thread this app creation, throw up a splash screen until done.
 		OregonTrail app = new OregonTrail();
+		app.open();
 	}
 
 	public OregonTrail() {
-		
 		this.mainMenu = new MainMenu(this);
-
-		//NewGameScreen test = new NewGameScreen(this);
 		frame = new JFrame("Oregon Trail");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setJMenuBar(mainMenu);
-		//frame.add(test);
 		frame.pack();
 		frame.setSize(new Dimension(600, 300));
+	}
+	
+	public void open(){
 		frame.setVisible(true);
+	}
+	
+	public void exit(){
+		frame.setVisible(false);
+		System.exit(0);
 	}
 
 	public void setGame(Game game) {
