0fe87a39da43c200efb1f5ed9f5053b1822a5713
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-10 13:40:19 -0400

    ShopScreen now inherits from LocationScreen

---
+++ src/com/kelsonprime/oregontrail/gui/LocationScreen.java
@@ -0,0 +1,8 @@
+package com.kelsonprime.oregontrail.gui;
+
+import javax.swing.JPanel;
+
+public abstract class LocationScreen extends JPanel{
+	private static final long serialVersionUID = 2247264002726443781L;
+	
+}
