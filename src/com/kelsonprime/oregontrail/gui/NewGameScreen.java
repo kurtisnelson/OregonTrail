4f6de51816818bdc0468a757976dc6ceaf21dc57
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-11 16:17:09 -0400

    Added variable names!

--- src/com/kelsonprime/oregontrail/gui/NewGameScreen.java
+++ src/com/kelsonprime/oregontrail/gui/NewGameScreen.java
@@ -1,25 +1,42 @@
 package com.kelsonprime.oregontrail.gui;
 
-import java.awt.*;
-import java.awt.event.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
-import com.kelsonprime.oregontrail.controller.*;
-import com.kelsonprime.oregontrail.model.*;
+import com.kelsonprime.oregontrail.controller.Game;
+import com.kelsonprime.oregontrail.model.Banker;
+import com.kelsonprime.oregontrail.model.Carpenter;
+import com.kelsonprime.oregontrail.model.Companion;
+import com.kelsonprime.oregontrail.model.Farmer;
+import com.kelsonprime.oregontrail.model.Occupation;
+import com.kelsonprime.oregontrail.model.Player;
+import com.kelsonprime.oregontrail.model.Wagon;
 
 public class NewGameScreen extends JPanel {
 	private static final long serialVersionUID = 7996950179577943594L;
 	OregonTrail app;
-	private JTextField txtPlayer;
-	private JTextField txtCompanion;
-	private JTextField txtCompanion_1;
-	private JTextField txtCompanion_2;
-	private JTextField txtCompanion_3;
+	private JTextField playerName;
+	private JTextField companionName;
+	private JTextField companion1Name;
+	private JTextField companion2Name;
+	private JTextField companion3Name;
 	private ButtonGroup occupationGroup;
-	private JLabel lblAbilitiesList;
-	private JLabel lblStartMoney;
+	private JLabel abilityList;
+	private JLabel startingMoney;
 
 	public NewGameScreen(OregonTrail app) {
 		super();
@@ -28,163 +45,161 @@ public class NewGameScreen extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 		setSize(new Dimension(600, 300));
 
-		JPanel panel = new JPanel();
-		panel.setBackground(UIManager
-				.getColor("OptionPane.warningDialog.titlePane.shadow"));
-		add(panel, BorderLayout.NORTH);
+		JPanel header = new JPanel();
+		header.setBackground(Color.white);
+		add(header, BorderLayout.NORTH);
 
-		JLabel lblGetReadyFor = new JLabel("Get Ready for the Oregon Trail!");
-		panel.add(lblGetReadyFor);
+		JLabel headerLabel = new JLabel("Get Ready for the Oregon Trail!");
+		header.add(headerLabel);
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setForeground(Color.WHITE);
-		panel_2.setBackground(UIManager
-				.getColor("OptionPane.warningDialog.titlePane.foreground"));
-		add(panel_2, BorderLayout.CENTER);
-		panel_2.setLayout(null);
+		JPanel body = new JPanel();
+		body.setForeground(Color.WHITE);
+		body.setBackground(Color.BLACK);
+		add(body, BorderLayout.CENTER);
+		body.setLayout(null);
 
-		txtPlayer = new JTextField();
-		txtPlayer.setBounds(9, 29, 114, 19);
-		txtPlayer.setText("Player 1");
-		panel_2.add(txtPlayer);
-		txtPlayer.setColumns(10);
+		playerName = new JTextField();
+		playerName.setBounds(9, 29, 114, 19);
+		playerName.setText("Player 1");
+		body.add(playerName);
+		playerName.setColumns(10);
 
-		txtCompanion = new JTextField();
-		txtCompanion.setBounds(9, 64, 114, 19);
-		txtCompanion.setText("Companion 1");
-		panel_2.add(txtCompanion);
-		txtCompanion.setColumns(10);
+		companionName = new JTextField();
+		companionName.setBounds(9, 64, 114, 19);
+		companionName.setText("Companion 1");
+		body.add(companionName);
+		companionName.setColumns(10);
 
-		txtCompanion_1 = new JTextField();
-		txtCompanion_1.setBounds(9, 96, 114, 19);
-		txtCompanion_1.setText("Companion 2");
-		panel_2.add(txtCompanion_1);
-		txtCompanion_1.setColumns(10);
+		companion1Name = new JTextField();
+		companion1Name.setBounds(9, 96, 114, 19);
+		companion1Name.setText("Companion 2");
+		body.add(companion1Name);
+		companion1Name.setColumns(10);
 
-		txtCompanion_2 = new JTextField();
-		txtCompanion_2.setBounds(9, 127, 114, 19);
-		txtCompanion_2.setText("Companion 3");
-		panel_2.add(txtCompanion_2);
-		txtCompanion_2.setColumns(10);
+		companion2Name = new JTextField();
+		companion2Name.setBounds(9, 127, 114, 19);
+		companion2Name.setText("Companion 3");
+		body.add(companion2Name);
+		companion2Name.setColumns(10);
 
-		txtCompanion_3 = new JTextField();
-		txtCompanion_3.setBounds(9, 158, 114, 19);
-		txtCompanion_3.setText("Companion 4");
-		panel_2.add(txtCompanion_3);
-		txtCompanion_3.setColumns(10);
+		companion3Name = new JTextField();
+		companion3Name.setBounds(9, 158, 114, 19);
+		companion3Name.setText("Companion 4");
+		body.add(companion3Name);
+		companion3Name.setColumns(10);
 
-		JLabel lblPickAnOccupation = new JLabel("Pick an Occupation");
-		lblPickAnOccupation.setFont(new Font("Dialog", Font.BOLD, 14));
-		lblPickAnOccupation.setForeground(Color.WHITE);
-		lblPickAnOccupation.setBounds(242, 4, 176, 15);
-		panel_2.add(lblPickAnOccupation);
+		JLabel occupationPickLabel = new JLabel("Pick an Occupation");
+		occupationPickLabel.setFont(new Font("Dialog", Font.BOLD, 14));
+		occupationPickLabel.setForeground(Color.WHITE);
+		occupationPickLabel.setBounds(242, 4, 176, 15);
+		body.add(occupationPickLabel);
 
 		ButtonListener listen = new ButtonListener();
 
-		JRadioButton rdbtnBanker = new JRadioButton("Banker");
-		rdbtnBanker.setBounds(178, 27, 75, 23);
-		rdbtnBanker.setActionCommand("banker");
-		rdbtnBanker.addActionListener(listen);
-		panel_2.add(rdbtnBanker);
+		JRadioButton bankerButton = new JRadioButton("Banker");
+		bankerButton.setBounds(178, 27, 75, 23);
+		bankerButton.setActionCommand("banker");
+		bankerButton.addActionListener(listen);
+		body.add(bankerButton);
 
-		JRadioButton rdbtnCarpenter = new JRadioButton("Carpenter");
-		rdbtnCarpenter.setBounds(269, 27, 97, 23);
-		rdbtnCarpenter.setActionCommand("carpenter");
-		rdbtnCarpenter.addActionListener(listen);
-		panel_2.add(rdbtnCarpenter);
+		JRadioButton carpenterButton = new JRadioButton("Carpenter");
+		carpenterButton.setBounds(269, 27, 97, 23);
+		carpenterButton.setActionCommand("carpenter");
+		carpenterButton.addActionListener(listen);
+		body.add(carpenterButton);
 
-		JRadioButton rdbtnFarmer = new JRadioButton("Farmer");
-		rdbtnFarmer.setBounds(385, 27, 75, 23);
-		rdbtnFarmer.setActionCommand("farmer");
-		rdbtnFarmer.addActionListener(listen);
-		panel_2.add(rdbtnFarmer);
+		JRadioButton farmerButton = new JRadioButton("Farmer");
+		farmerButton.setBounds(385, 27, 75, 23);
+		farmerButton.setActionCommand("farmer");
+		farmerButton.addActionListener(listen);
+		body.add(farmerButton);
 
 		occupationGroup = new ButtonGroup();
-		occupationGroup.add(rdbtnBanker);
-		occupationGroup.add(rdbtnCarpenter);
-		occupationGroup.add(rdbtnFarmer);
+		occupationGroup.add(bankerButton);
+		occupationGroup.add(carpenterButton);
+		occupationGroup.add(farmerButton);
 
-		JLabel lblPartyNames = new JLabel("Party Names");
-		lblPartyNames.setFont(new Font("Dialog", Font.BOLD, 14));
-		lblPartyNames.setForeground(Color.WHITE);
-		lblPartyNames.setBounds(9, 2, 114, 15);
-		panel_2.add(lblPartyNames);
+		JLabel nameLabel = new JLabel("Party Names");
+		nameLabel.setFont(new Font("Dialog", Font.BOLD, 14));
+		nameLabel.setForeground(Color.WHITE);
+		nameLabel.setBounds(9, 2, 114, 15);
+		body.add(nameLabel);
 
-		JLabel lblMoney = new JLabel("Starting Money:");
-		lblMoney.setForeground(Color.WHITE);
-		lblMoney.setBounds(184, 112, 117, 15);
-		panel_2.add(lblMoney);
+		JLabel moneyLabel = new JLabel("Starting Money:");
+		moneyLabel.setForeground(Color.WHITE);
+		moneyLabel.setBounds(184, 112, 117, 15);
+		body.add(moneyLabel);
 
-		JLabel lblOccupationalAbilities = new JLabel("Occupational Abilities:");
-		lblOccupationalAbilities.setForeground(Color.WHITE);
-		lblOccupationalAbilities.setBounds(183, 66, 262, 15);
-		panel_2.add(lblOccupationalAbilities);
+		JLabel abilityLabel = new JLabel("Occupational Abilities:");
+		abilityLabel.setForeground(Color.WHITE);
+		abilityLabel.setBounds(183, 66, 262, 15);
+		body.add(abilityLabel);
 
-		JLabel lblSetStartingConditions = new JLabel("Set Starting Conditions");
-		lblSetStartingConditions.setFont(new Font("Dialog", Font.BOLD, 14));
-		lblSetStartingConditions.setForeground(Color.WHITE);
-		lblSetStartingConditions.setBounds(109, 189, 192, 15);
-		panel_2.add(lblSetStartingConditions);
+		JLabel startingConditionLabel = new JLabel("Set Starting Conditions");
+		startingConditionLabel.setFont(new Font("Dialog", Font.BOLD, 14));
+		startingConditionLabel.setForeground(Color.WHITE);
+		startingConditionLabel.setBounds(109, 189, 192, 15);
+		body.add(startingConditionLabel);
 
-		JLabel lblPace = new JLabel("Pace");
-		lblPace.setForeground(Color.WHITE);
-		lblPace.setBounds(100, 210, 70, 15);
-		panel_2.add(lblPace);
+		JLabel paceLabel = new JLabel("Pace");
+		paceLabel.setForeground(Color.WHITE);
+		paceLabel.setBounds(100, 210, 70, 15);
+		body.add(paceLabel);
 
-		JLabel lblRationing = new JLabel("Rations");
-		lblRationing.setForeground(Color.WHITE);
-		lblRationing.setBounds(269, 210, 70, 15);
-		panel_2.add(lblRationing);
+		JLabel rationingLabel = new JLabel("Rations");
+		rationingLabel.setForeground(Color.WHITE);
+		rationingLabel.setBounds(269, 210, 70, 15);
+		body.add(rationingLabel);
 
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(62, 226, 124, 24);
-		panel_2.add(comboBox);
-		comboBox.addItem("Light");
-		comboBox.addItem("Moderate");
-		comboBox.addItem("Heavy");
-		comboBox.addItem("Grueling");
+		JComboBox paceBox = new JComboBox();
+		paceBox.setBounds(62, 226, 124, 24);
+		body.add(paceBox);
+		paceBox.addItem("Light");
+		paceBox.addItem("Moderate");
+		paceBox.addItem("Heavy");
+		paceBox.addItem("Grueling");
 
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(228, 226, 124, 24);
-		panel_2.add(comboBox_1);
-		comboBox_1.addItem("Bare Bones");
-		comboBox_1.addItem("Meager");
-		comboBox_1.addItem("Adequate");
-		comboBox_1.addItem("Filling");
+		JComboBox rationBox = new JComboBox();
+		rationBox.setBounds(228, 226, 124, 24);
+		body.add(rationBox);
+		rationBox.addItem("Bare Bones");
+		rationBox.addItem("Meager");
+		rationBox.addItem("Adequate");
+		rationBox.addItem("Filling");
 
-		JButton btnContinue = new JButton("Continue -->");
-		btnContinue.setBounds(430, 226, 144, 25);
-		btnContinue.setActionCommand("continue");
-		btnContinue.addActionListener(listen);
-		panel_2.add(btnContinue);
+		JButton continueButton = new JButton("Continue");
+		continueButton.setBounds(430, 226, 144, 25);
+		continueButton.setActionCommand("continue");
+		continueButton.addActionListener(listen);
+		body.add(continueButton);
 
-		lblAbilitiesList = new JLabel("---");
-		lblAbilitiesList.setFont(new Font("Dialog", Font.PLAIN, 12));
-		lblAbilitiesList.setForeground(Color.WHITE);
-		lblAbilitiesList.setBounds(193, 85, 282, 15);
-		panel_2.add(lblAbilitiesList);
+		abilityList = new JLabel("---");
+		abilityList.setFont(new Font("Dialog", Font.PLAIN, 12));
+		abilityList.setForeground(Color.WHITE);
+		abilityList.setBounds(193, 85, 282, 15);
+		body.add(abilityList);
 
-		lblStartMoney = new JLabel("---");
-		lblStartMoney.setFont(new Font("Dialog", Font.PLAIN, 12));
-		lblStartMoney.setForeground(Color.WHITE);
-		lblStartMoney.setBounds(194, 129, 145, 15);
-		panel_2.add(lblStartMoney);
+		startingMoney = new JLabel("---");
+		startingMoney.setFont(new Font("Dialog", Font.PLAIN, 12));
+		startingMoney.setForeground(Color.WHITE);
+		startingMoney.setBounds(194, 129, 145, 15);
+		body.add(startingMoney);
 	}
 
 	public void createGame() {
 
 		ArrayList<Companion> companionList = new ArrayList<Companion>(4);
-		if (txtCompanion.getText() != null) {
-			companionList.add(new Companion(txtCompanion.getText()));
+		if (companionName.getText() != null) {
+			companionList.add(new Companion(companionName.getText()));
 		}
-		if (txtCompanion_1.getText() != null) {
-			companionList.add(new Companion(txtCompanion_1.getText()));
+		if (companion1Name.getText() != null) {
+			companionList.add(new Companion(companion1Name.getText()));
 		}
-		if (txtCompanion_2.getText() != null) {
-			companionList.add(new Companion(txtCompanion_2.getText()));
+		if (companion2Name.getText() != null) {
+			companionList.add(new Companion(companion2Name.getText()));
 		}
-		if (txtCompanion_3.getText() != null) {
-			companionList.add(new Companion(txtCompanion_3.getText()));
+		if (companion3Name.getText() != null) {
+			companionList.add(new Companion(companion3Name.getText()));
 		}
 
 		String occString = occupationGroup.getSelection().getActionCommand();
@@ -198,7 +213,7 @@ public class NewGameScreen extends JPanel {
 			newOccupation = new Farmer();
 		}
 
-		Player newPlayer = new Player(txtPlayer.getText(), newOccupation);
+		Player newPlayer = new Player(playerName.getText(), newOccupation);
 
 		Wagon newWagon = new Wagon(newPlayer, companionList);
 
@@ -213,14 +228,14 @@ public class NewGameScreen extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 			Object o = e.getActionCommand();
 			if (o.equals("banker")) {
-				lblAbilitiesList.setText("-None-");
-				lblStartMoney.setText("$3000");
+				abilityList.setText("-None-");
+				startingMoney.setText("$3000");
 			} else if (o.equals("carpenter")) {
-				lblAbilitiesList.setText("Wagon Fixing");
-				lblStartMoney.setText("$2000");
+				abilityList.setText("Wagon Fixing");
+				startingMoney.setText("$2000");
 			} else if (o.equals("farmer")) {
-				lblAbilitiesList.setText("Oxen Care | Food Scavaging");
-				lblStartMoney.setText("$1500");
+				abilityList.setText("Oxen Care | Food Scavaging");
+				startingMoney.setText("$1500");
 			} else if (o.equals("continue")) {
 				createGame();
 			}
