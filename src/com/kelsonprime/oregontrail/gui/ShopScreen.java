02c63ef3f0d8c50883dfa6c5329fb4612c63ca2b
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-11 16:08:25 -0400

    Named the variables!

--- src/com/kelsonprime/oregontrail/gui/ShopScreen.java
+++ src/com/kelsonprime/oregontrail/gui/ShopScreen.java
@@ -1,23 +1,31 @@
 package com.kelsonprime.oregontrail.gui;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
-import com.kelsonprime.oregontrail.model.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
+import com.kelsonprime.oregontrail.model.Shop;
+import com.kelsonprime.oregontrail.model.Wagon;
 
 public class ShopScreen extends LocationScreen {
 	private static final long serialVersionUID = -7550296665413775236L;
 	private static final String CURRENCY = "$";
 	private Shop shop;
-	private JTextField txtNumoxen;
-	private JTextField txtNumAxles;
-	private JTextField txtNumwheels;
-	private JTextField txtNumtongues;
-	private JTextField txtNumclothes;
-	private JTextField txtNumbullets;
-	private JTextField txtNumfood;
+	private JTextField oxenQuantity;
+	private JTextField axleQuantity;
+	private JTextField wheelQuantity;
+	private JTextField tongueQuantity;
+	private JTextField clothesQuantity;
+	private JTextField bulletQuantity;
+	private JTextField foodQuantity;
 
 	public ShopScreen(Wagon wagon, Shop shop) {
 		super(wagon);
@@ -25,151 +33,151 @@ public class ShopScreen extends LocationScreen {
 		setSize(new Dimension(600, 300));
 		setLayout(new BorderLayout(0, 0));
 
-		JPanel panel = new JPanel();
-		panel.setBackground(UIManager
+		JPanel header = new JPanel();
+		header.setBackground(UIManager
 				.getColor("OptionPane.warningDialog.titlePane.foreground"));
-		panel.setForeground(UIManager
+		header.setForeground(UIManager
 				.getColor("OptionPane.warningDialog.titlePane.foreground"));
-		add(panel, BorderLayout.NORTH);
+		add(header, BorderLayout.NORTH);
 
-		JLabel lblWelcomeToThe = new JLabel("Welcome to the " + shop.label()
+		JLabel screenLabel = new JLabel("Welcome to the " + shop.label()
 				+ " Shop");
-		lblWelcomeToThe.setForeground(Color.WHITE);
-		panel.add(lblWelcomeToThe);
+		screenLabel.setLabelFor(header);
+		screenLabel.setForeground(Color.WHITE);
+		header.add(screenLabel);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(UIManager
+		JPanel body = new JPanel();
+		body.setBackground(UIManager
 				.getColor("OptionPane.warningDialog.titlePane.shadow"));
-		panel_1.setForeground(UIManager
+		body.setForeground(UIManager
 				.getColor("OptionPane.warningDialog.titlePane.shadow"));
-		add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(null);
+		add(body, BorderLayout.CENTER);
+		body.setLayout(null);
 
-		txtNumAxles = new JTextField();
-		txtNumAxles.setText("0");
-		txtNumAxles.setBounds(410, 43, 88, 19);
-		panel_1.add(txtNumAxles);
-		txtNumAxles.setColumns(10);
+		axleQuantity = new JTextField();
+		axleQuantity.setText("0");
+		axleQuantity.setBounds(410, 43, 88, 19);
+		body.add(axleQuantity);
+		axleQuantity.setColumns(10);
 
-		txtNumoxen = new JTextField();
-		txtNumoxen.setText("0");
-		txtNumoxen.setBounds(410, 12, 88, 19);
-		panel_1.add(txtNumoxen);
-		txtNumoxen.setColumns(10);
+		oxenQuantity = new JTextField();
+		oxenQuantity.setText("0");
+		oxenQuantity.setBounds(410, 12, 88, 19);
+		body.add(oxenQuantity);
+		oxenQuantity.setColumns(10);
 
-		txtNumwheels = new JTextField();
-		txtNumwheels.setText("0");
-		txtNumwheels.setBounds(410, 74, 88, 19);
-		panel_1.add(txtNumwheels);
-		txtNumwheels.setColumns(10);
+		wheelQuantity = new JTextField();
+		wheelQuantity.setText("0");
+		wheelQuantity.setBounds(410, 74, 88, 19);
+		body.add(wheelQuantity);
+		wheelQuantity.setColumns(10);
 
-		txtNumtongues = new JTextField();
-		txtNumtongues.setText("0");
-		txtNumtongues.setBounds(410, 105, 88, 19);
-		panel_1.add(txtNumtongues);
-		txtNumtongues.setColumns(10);
+		tongueQuantity = new JTextField();
+		tongueQuantity.setText("0");
+		tongueQuantity.setBounds(410, 105, 88, 19);
+		body.add(tongueQuantity);
+		tongueQuantity.setColumns(10);
 
-		txtNumclothes = new JTextField();
-		txtNumclothes.setText("0");
-		txtNumclothes.setBounds(410, 136, 88, 19);
-		panel_1.add(txtNumclothes);
-		txtNumclothes.setColumns(10);
+		clothesQuantity = new JTextField();
+		clothesQuantity.setText("0");
+		clothesQuantity.setBounds(410, 136, 88, 19);
+		body.add(clothesQuantity);
+		clothesQuantity.setColumns(10);
 
-		txtNumbullets = new JTextField();
-		txtNumbullets.setText("0");
-		txtNumbullets.setBounds(410, 168, 88, 19);
-		panel_1.add(txtNumbullets);
-		txtNumbullets.setColumns(10);
+		bulletQuantity = new JTextField();
+		bulletQuantity.setText("0");
+		bulletQuantity.setBounds(410, 168, 88, 19);
+		body.add(bulletQuantity);
+		bulletQuantity.setColumns(10);
 
-		txtNumfood = new JTextField();
-		txtNumfood.setText("0");
-		txtNumfood.setBounds(410, 201, 88, 19);
-		panel_1.add(txtNumfood);
-		txtNumfood.setColumns(10);
+		foodQuantity = new JTextField();
+		foodQuantity.setText("0");
+		foodQuantity.setBounds(410, 201, 88, 19);
+		body.add(foodQuantity);
+		foodQuantity.setColumns(10);
 
-		JLabel lblOxen = new JLabel("Oxen");
-		lblOxen.setBounds(375, 16, 41, 15);
-		panel_1.add(lblOxen);
+		JLabel oxenLabel = new JLabel("Oxen");
+		oxenLabel.setBounds(375, 16, 41, 15);
+		body.add(oxenLabel);
 
-		JLabel lblAxle = new JLabel("Axle");
-		lblAxle.setBounds(382, 45, 34, 15);
-		panel_1.add(lblAxle);
+		JLabel axleLabel = new JLabel("Axle");
+		axleLabel.setBounds(382, 45, 34, 15);
+		body.add(axleLabel);
 
-		JLabel lblWheels = new JLabel("Wheels");
-		lblWheels.setBounds(358, 75, 53, 17);
-		panel_1.add(lblWheels);
+		JLabel wheelLabel = new JLabel("Wheels");
+		wheelLabel.setBounds(358, 75, 53, 17);
+		body.add(wheelLabel);
 
-		JLabel lblTongues = new JLabel("Tongues");
-		lblTongues.setBounds(349, 107, 61, 15);
-		panel_1.add(lblTongues);
+		JLabel tongueLabel = new JLabel("Tongues");
+		tongueLabel.setBounds(349, 107, 61, 15);
+		body.add(tongueLabel);
 
-		JLabel lblClothes = new JLabel("Clothes");
-		lblClothes.setBounds(355, 138, 61, 15);
-		panel_1.add(lblClothes);
+		JLabel clothLabel = new JLabel("Clothes");
+		clothLabel.setBounds(355, 138, 61, 15);
+		body.add(clothLabel);
 
-		JLabel lblBullets = new JLabel("Bullets (10/box)");
-		lblBullets.setBounds(302, 170, 108, 15);
-		panel_1.add(lblBullets);
+		JLabel bulletsLabel = new JLabel("Bullets (10/box)");
+		bulletsLabel.setBounds(302, 170, 108, 15);
+		body.add(bulletsLabel);
 
-		JLabel lblFood = new JLabel("Food (lbs)");
-		lblFood.setBounds(340, 203, 70, 15);
-		panel_1.add(lblFood);
+		JLabel foodLabel = new JLabel("Food (lbs)");
+		foodLabel.setBounds(340, 203, 70, 15);
+		body.add(foodLabel);
 
-		JLabel lblAxleprice = new JLabel(CURRENCY + shop.axlePrice());
-		lblAxleprice.setBounds(506, 45, 61, 15);
-		panel_1.add(lblAxleprice);
+		JLabel axlePrice = new JLabel(CURRENCY + shop.axlePrice());
+		axlePrice.setBounds(506, 45, 61, 15);
+		body.add(axlePrice);
 
-		JLabel lblWheelprice = new JLabel(CURRENCY + shop.wheelPrice());
-		lblWheelprice.setBounds(505, 76, 61, 15);
-		panel_1.add(lblWheelprice);
+		JLabel wheelPrice = new JLabel(CURRENCY + shop.wheelPrice());
+		wheelPrice.setBounds(505, 76, 61, 15);
+		body.add(wheelPrice);
 
-		JLabel lblOxenprice = new JLabel(CURRENCY + shop.oxenPrice());
-		lblOxenprice.setBounds(506, 14, 70, 15);
-		panel_1.add(lblOxenprice);
+		JLabel oxenPrice = new JLabel(CURRENCY + shop.oxenPrice());
+		oxenPrice.setBounds(506, 14, 70, 15);
+		body.add(oxenPrice);
 
-		JLabel lblTongueprice = new JLabel(CURRENCY + shop.tonguePrice());
-		lblTongueprice.setBounds(506, 107, 70, 15);
-		panel_1.add(lblTongueprice);
+		JLabel tonguePrice = new JLabel(CURRENCY + shop.tonguePrice());
+		tonguePrice.setBounds(506, 107, 70, 15);
+		body.add(tonguePrice);
 
-		JLabel lblClothesprice = new JLabel(CURRENCY + shop.clothesPrice());
-		lblClothesprice.setBounds(506, 138, 70, 15);
-		panel_1.add(lblClothesprice);
+		JLabel clothesPrice = new JLabel(CURRENCY + shop.clothesPrice());
+		clothesPrice.setBounds(506, 138, 70, 15);
+		body.add(clothesPrice);
 
-		JLabel lblBulletprice = new JLabel(CURRENCY + shop.bulletPrice());
-		lblBulletprice.setBounds(506, 170, 70, 15);
-		panel_1.add(lblBulletprice);
+		JLabel bulletPrice = new JLabel(CURRENCY + shop.bulletPrice());
+		bulletPrice.setBounds(506, 170, 70, 15);
+		body.add(bulletPrice);
 
-		JLabel lblFoodprice = new JLabel(CURRENCY + shop.foodPrice());
-		lblFoodprice.setBounds(506, 203, 70, 15);
-		panel_1.add(lblFoodprice);
+		JLabel foodPrice = new JLabel(CURRENCY + shop.foodPrice());
+		foodPrice.setBounds(506, 203, 70, 15);
+		body.add(foodPrice);
 
-		JButton btnBuyItems = new JButton("Buy Items");
-		btnBuyItems.setBounds(320, 232, 117, 25);
-		panel_1.add(btnBuyItems);
-		btnBuyItems.setActionCommand("buy");
+		JButton buyButton = new JButton("Buy Items");
+		buyButton.setBounds(320, 232, 117, 25);
+		body.add(buyButton);
+		buyButton.setActionCommand("buy");
 
-		JButton btnLeaveShop = new JButton("Leave Shop");
-		btnLeaveShop.setBounds(450, 232, 117, 25);
-		panel_1.add(btnLeaveShop);
-		btnLeaveShop.setActionCommand("leave");
+		JButton leaveButton = new JButton("Leave Shop");
+		leaveButton.setBounds(450, 232, 117, 25);
+		body.add(leaveButton);
+		leaveButton.setActionCommand("leave");
 
-		JLabel lblMoney = new JLabel("Money: $" + Integer.toString(wagon.getMoney()));
-		lblMoney.setBounds(12, 14, 154, 15);
-		panel_1.add(lblMoney);
+		JLabel moneyLabel = new JLabel("Money: $" + wagon.getMoney());
+		moneyLabel.setBounds(12, 14, 154, 15);
+		body.add(moneyLabel);
 	}
-	
-	private class ButtonListener implements ActionListener{
+
+	private class ButtonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent ae) {
 			String s = ae.getActionCommand();
-			if (s == "buy"){
-				
-			}
-			else if(s == "leave"){
-				
+			if (s.equals("buy")) {
+				// TODO mutate Wagon based on Shop
+			} else if (s.equals("leave")) {
+
 			}
 		}
-		
+
 	}
 }
