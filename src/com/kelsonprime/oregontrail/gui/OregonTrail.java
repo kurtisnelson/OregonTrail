c96af80f87704be36a45547cbd6e07bf0223381f
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-19 12:47:15 -0400

    Created Threader to allow consistant thread pools to be used across the app.

--- src/com/kelsonprime/oregontrail/gui/OregonTrail.java
+++ src/com/kelsonprime/oregontrail/gui/OregonTrail.java
@@ -16,6 +16,7 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import com.kelsonprime.oregontrail.controller.Game;
+import com.kelsonprime.oregontrail.controller.Threader;
 import com.kelsonprime.oregontrail.controller.UserProperties;
 import com.kelsonprime.oregontrail.model.Location;
 import com.kelsonprime.oregontrail.model.Shop;
@@ -61,10 +62,9 @@ public class OregonTrail {
 		LOGGER.setUseParentHandlers(false);
 		LOGGER.log(Level.INFO, "App started");
 		
-		// TODO throw splash screen?
 		 //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		Threader.executeNow(new Runnable() {
             public void run() {
             	OregonTrail app = new OregonTrail();
             	app.open();
@@ -85,8 +85,7 @@ public class OregonTrail {
 		frame.setIconImage(new ImageIcon("images/OregonTrailIcon.png").getImage());
 		frame.setResizable(false);
 		frame.setUndecorated(true);
-		JPanel tempPanel = new SplashScreen();
-		setPanel(tempPanel);
+		setPanel(new SplashScreen());
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(new Dimension(600, 350));
