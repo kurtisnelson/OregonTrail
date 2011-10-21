cc76bdaeee93248118e12405d3f23392ee829b2a
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-21 16:49:53 -0400

    javadocs

--- src/com/kelsonprime/oregontrail/controller/Time.java
+++ src/com/kelsonprime/oregontrail/controller/Time.java
@@ -1,11 +1,20 @@
 package com.kelsonprime.oregontrail.controller;
 
+/**
+ * Allow objects to change and mutate with time.
+ * @author Kurt Nelson
+ * @version 1.0
+ */
 public interface Time {
+	
 	/**
 	 * Eat, age items, adjust heath, rest, and increment the day.
-	 * @param game Current game state
 	 */
 	public void nextDay();
 	
+	/**
+	 * Checks to see if the object is usable
+	 * @return true if it is ready
+	 */
 	public boolean isReady();
 }
