5fd7f2f0857d75c7ec6fe5dc81139a68fb003c54
Matt Hawkins <trumpet580@gmail.com>
2011-10-21 18:50:11 -0400

    Finished implementing nextDay method.

--- src/com/kelsonprime/oregontrail/model/Wagon.java
+++ src/com/kelsonprime/oregontrail/model/Wagon.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import com.kelsonprime.oregontrail.controller.Game;
+import com.kelsonprime.oregontrail.controller.Ration;
 import com.kelsonprime.oregontrail.controller.Time;
 import com.kelsonprime.oregontrail.controller.UserInputException;
 
@@ -75,8 +76,18 @@ public class Wagon implements Time {
 			part.nextDay(game);
 		}
 		repair();
-		//food
 		
+		Ration ration = game.getRation();
+		switch (ration) {
+		case BARE: food -= 1;
+		case MEAGER: food -= 3;
+		case NORMAL: food -= 5;
+		case WELLFED: food -= 7;
+		}
+		if (food < 0 ){
+			food = 0;
+		}
+		//TODO whatever we decide to do with clothes, probably should do something here
 	}
 
 	/**
