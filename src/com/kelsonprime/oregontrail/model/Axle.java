87b482454722393bf9d805f6a89e057146b9b86b
Aman <Aman@Aman-PC>
2011-10-23 22:06:30 -0400

    javadoc'd Axle

--- src/com/kelsonprime/oregontrail/model/Axle.java
+++ src/com/kelsonprime/oregontrail/model/Axle.java
@@ -2,16 +2,30 @@ package com.kelsonprime.oregontrail.model;
 
 import com.kelsonprime.oregontrail.controller.Game;
 
+/**
+ * Represents an Axle. Inherits from Part
+ * @author Aman
+ *
+ */
 public class Axle extends Part {
 
+	/**
+	 * Constructor without arguments
+	 */
 	public Axle(){
 		this(100);
 	}
 	
+	/**
+	 * Constructor with health given
+	 */
 	public Axle(int health) {
 		super(health, 5);
 	}
 	
+	/**
+	 * Gets the name of the part in string form
+	 */
 	public String toString() {
 		return "Axle";
 	}
