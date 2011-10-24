1a2a0dcbf097b1209ba809526dc13cf823c0b6b9
Aman <Aman@Aman-PC>
2011-10-23 21:44:55 -0400

    javadoc'd IconFactory

--- src/com/kelsonprime/oregontrail/controller/IconFactory.java
+++ src/com/kelsonprime/oregontrail/controller/IconFactory.java
@@ -2,6 +2,11 @@ package com.kelsonprime.oregontrail.controller;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Stores an arrow icon
+ * @author Aman
+ *
+ */
 public final class IconFactory {
 	public static final ImageIcon arrow = new ImageIcon("images/arrow.png");
 }
