ca4e29b36b604b92acfa6751ba82f9ac0af1955d
Aman <Aman@Aman-PC>
2011-10-23 21:37:21 -0400

    Events javadoc

--- src/com/kelsonprime/oregontrail/controller/Events.java
+++ src/com/kelsonprime/oregontrail/controller/Events.java
@@ -1,15 +1,27 @@
 package com.kelsonprime.oregontrail.controller;
 
+/**
+ * Generates random events for the game
+ */
 class Events {
 
+	/**
+	 * Generated death
+	 */
 	void death() {
 		// TODO method stub
 	}
 
+	/**
+	 * Randomly calculate an event
+	 */
 	void calculateRandomEvent() {
 
 	}
 
+	/**
+	 * Generates next day
+	 */
 	void nextDay() {
 		// TODO method stub
 	}
