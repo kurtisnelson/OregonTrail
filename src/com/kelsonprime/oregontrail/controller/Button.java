cb57611fdd2f5068419342e9d0b7f4cb94e6583e
jgifpeg <jgifpeg@yahoo.com>
2011-10-26 11:33:41 -0400

    super'd Button class so should be better

--- src/com/kelsonprime/oregontrail/controller/Button.java
+++ src/com/kelsonprime/oregontrail/controller/Button.java
@@ -1,5 +1,6 @@
 package com.kelsonprime.oregontrail.controller;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
@@ -8,14 +9,18 @@ import javax.swing.JButton;
 
 public class Button extends JButton {
 	
+	private static final long serialVersionUID = 1L;
 	private ImageIcon buttonImage;
 	
 	public Button(ImageIcon img){
+		super();
 		buttonImage = img;
 		setPreferredSize(new Dimension(100, 50));
+		setBackground(new Color(0,0,0,0));
 	}
 	
 	public void paintComponent(Graphics g){
+		super.paintComponent(g);
 		g.drawImage(buttonImage.getImage(), 0, 0, this);
 	}
 }
