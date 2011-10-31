2878ee7a1c4b768a9f0825803bd04734987f12fa
Kurt Nelson <kurtisnelson@gmail.com>
2011-10-31 13:55:53 -0400

    Added javadocs on Crossing stub methods

--- src/com/kelsonprime/oregontrail/model/Crossing.java
+++ src/com/kelsonprime/oregontrail/model/Crossing.java
@@ -6,6 +6,13 @@ public class Crossing extends Location {
 	private int width;
 	private int ferryCost;
 
+	/**
+	 * Initialize a crossing on the map
+	 * @param name
+	 * @param position Distance from map start
+	 * @param depth
+	 * @param width
+	 */
 	public Crossing(String name, int position, int depth, int width) {
 		super(name, position);
 		this.depth = depth;
@@ -23,18 +30,37 @@ public class Crossing extends Location {
 		return ret.toString();
 	}
 	
+	/**
+	 * Ford this crossing
+	 * @return Description of what happened
+	 * @post Wagon has been updated with anything bad that happened.
+	 */
 	public String ford() {
 		return null;
 	}
 
+	/**
+	 * Caulk and float at this crossing
+	 * @return Description of what happened
+	 * @post Wagon has been updated with anything bad that happened.
+	 */
 	public String caulk() {
 		return null;
 	}
 
+	/**
+	 * Check if a ferry is available.
+	 * @return is there a ferry
+	 */
 	public boolean isFerry(){
 		return ferryCost > 0;
 	}
 
+	/**
+	 * Take the ferry
+	 * @return Description of what happened
+	 * @post Wagon has been updated with anything bad that happened and cost spent
+	 */
 	public String ferry() {
 		if(ferryCost <= 0)
 			return "There is no ferry here.";
