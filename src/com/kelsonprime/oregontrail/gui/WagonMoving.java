e26872a7049a6b13ac41af0f3983b1f00aa8e55e
Victoria Ayo <vayo3@gatech.edu>
2011-10-31 22:32:42 -0400

    added wagon image

--- src/com/kelsonprime/oregontrail/gui/WagonMoving.java
+++ src/com/kelsonprime/oregontrail/gui/WagonMoving.java
@@ -7,6 +7,8 @@ import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JTextField;
 import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Panel;
@@ -15,12 +17,14 @@ public class WagonMoving {
 	OregonTrail app;
 	JPanel screen1;
 	private JTextField txtOregonTrail;
+	private JLabel lblNewLabel;
 	public WagonMoving(OregonTrail app){
 		super();
 		this.app = app;
 		//this.wagon = app.getWagon();
 		//setSize(new Dimension(600, 300));
 		screen1 = new JPanel();
+	
 		screen1.setBorder(new LineBorder(Color.BLACK, 3));
 		screen1.setSize(400, 300);
 		screen1.setLayout(new BorderLayout(0, 0));
@@ -35,6 +39,12 @@ public class WagonMoving {
 		
 		panel.setBackground(new Color(135, 206, 235));
 		screen1.add(panel, BorderLayout.CENTER);
+		
+		lblNewLabel = new JLabel(new ImageIcon("C:\\Users\\Victoria\\workspace\\OregonTrail\\images\\CoveredWagon.jpg"));
+		//lblNewLabel.setHorizontalAlignment(JLabel.CENTER);
+		//lblNewLabel.add(label);
+		
+		panel.add(lblNewLabel);
 	}
 
 }
