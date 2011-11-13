24f2b24ec2ab283d59c6a5f7c524151541fb2937
Kurt Nelson <kurtisnelson@gmail.com>
2011-11-13 16:10:25 -0500

    Fixed broken button

--- src/com/kelsonprime/oregontrail/controller/IconFactory.java
+++ src/com/kelsonprime/oregontrail/controller/IconFactory.java
@@ -9,14 +9,11 @@ import javax.swing.ImageIcon;
  * @version $Revision: 1.0 $
  */
 public final class IconFactory {
-	private static ImageIcon arrow;
 	/**
 	 * Arrow icon
 	 * @return ImageIcon
 	 */
 	public static final ImageIcon arrow(){
-		if(arrow == null)
-			arrow = new ImageIcon("images/arrow.png");
-		return arrow;
+		return new ImageIcon(IconFactory.class.getResource("/images/arrow.png"));
 	}
 }
