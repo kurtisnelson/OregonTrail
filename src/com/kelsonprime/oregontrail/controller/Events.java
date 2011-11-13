bc0f0308ee79427e30994872e1a73ef288e676db
jgifpeg <jgifpeg@yahoo.com>
2011-11-13 15:32:56 -0500

    Events readied for implementation.  TODO: Actual implementation.

--- src/com/kelsonprime/oregontrail/controller/Events.java
+++ src/com/kelsonprime/oregontrail/controller/Events.java
@@ -2,6 +2,8 @@ package com.kelsonprime.oregontrail.controller;
 
 import javax.swing.JOptionPane;
 
+import java.util.Random;
+
 import com.kelsonprime.oregontrail.model.Companion;
 import com.kelsonprime.oregontrail.model.Wagon;
 
@@ -10,6 +12,7 @@ import com.kelsonprime.oregontrail.model.Wagon;
  */
 public class Events {
 
+	private static Random rand;
 	/**
 	 * Generated death
 	 */
@@ -29,7 +32,29 @@ public class Events {
 	 * Generates next day
 	 */
 	public static void nextDay() {
-		// TODO method stub
+		int event = rand.nextInt(200);
+		if (event < 4)
+			;//Catch Sickness
+		else if (event < 6)
+			;//Random Part Breakdown
+		else if (event < 7)
+			;//Random Party member Death
+		else if (event < 8)
+			;//Random Oxen Death
+		else if (event < 11)
+			;//Theft
+		else if (event < 12)
+			;//Attacked
+		else if (event < 17)
+			;//Find Food (only farmer)
+		else if (event < 18)
+			;//Find Wagon of free stuff
+		else if (event < 20)
+			;//Oxen Weak (Pace halved)
+		else if (event < 22)
+			;//Random Party member recovery (health XOR sickness)
+		else if (event < 25)
+			;//Storm lose days
 	}
 	
 	public static void loseItems(Wagon wagon, double percent){
